10d1ac8d25958fd152c5ced629d736f3732ead42
Thomas Cowan <67574099+thomascowan@users.noreply.github.com>
2021-03-03 16:29:51 +1100

    Gives notification that password has been copied to clipboard
    Notification about password being copied to clipboard. Also implemented boolean to allow duplicate character entry next to each other.

--- gen.js
+++ gen.js
@@ -1,5 +1,6 @@
-// content.js
-chrome.browserAction.onClicked.addListener(function(tab) {
+// var app = chrome.runtime.getBackgroundPage();
+
+function work() {
     //Set length of the string
     const length = 12;
 
@@ -8,6 +9,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     const upper_case = true;
     const numbers = true;
     const symbols = false;
+    const noDuplicates = true;
 
     //increase the likelihood of character group appearing (minimum 1)
     const lower_case_weighting = 1;
@@ -24,12 +26,17 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     
     //Construct password according random math input
     var str = "";
+    var idx = Math.floor(Math.random() * charSet.length);
+    var oldIdx = -1;
     for(var i = 0; i < length; i++){
-        str += charSet[Math.floor(Math.random() * charSet.length)];
+        idx = Math.floor(Math.random() * charSet.length);
+        while(idx == oldIdx && noDuplicates) idx = Math.floor(Math.random() * charSet.length);
+        str += charSet[idx];
+        oldIdx = idx;
     }
     //copy password to clipboard
     copyToClipboard(str);
-});
+};
 
 const copyToClipboard = str => {
     const el = document.createElement('textarea');
@@ -42,3 +49,15 @@ const copyToClipboard = str => {
     document.execCommand('copy');
     document.body.removeChild(el);
 };
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+
+  
+document.getElementsByTagName("body").addEventListener('load', work());
